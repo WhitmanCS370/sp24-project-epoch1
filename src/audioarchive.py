c5eb0c88f6a8c7de55ad1cd0c3211e7a349ac758
Jordan Wirfs-Brock <jordanwb@gmail.com>
2024-02-12 10:45:36 -0800

    Added src, tests, sounds

---
+++ src/audioarchive.py
@@ -0,0 +1,15 @@
+
+'''Include comments at the top of each file that clearly say  what each file does.
+Note that you may structure your code so that it is separated into
+multiple files (i.e. modules), but that is not required.
+
+Remember, if you are splitting your files into multiple modules, you will need to make sure to use
+this convention:
+
+if __name__ == "__main__":
+    # Do something here
+    
+You can read more about that at: https://www.freecodecamp.org/news/if-name-main-python-example/
+'''
+
+# MAKE SURE TO INCLUDE ANY NECESSARY IMPORT STATEMENTS
---
+++ tests/tests.py
@@ -0,0 +1,17 @@
+
+'''
+If you have any tests, you may consider putting them in a separate file.
+You may use this structure.
+'''
+
+
+import unittest
+
+
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(True, False)  # add assertion here
+
+
+if __name__ == '__main__':
+    unittest.main()
